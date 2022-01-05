6685f01f3ab09e9594b8b4cd070508d8cbbfb0cf
Hyerim926 <ekfrhsk96@icloud.com>
2022-01-05 23:59:05 +0900

    ch5 jsondeepcopy 주석 추가

--- 01_데이터 타입/05_불변 객체/JSONDeepCopy.js
+++ 01_데이터 타입/05_불변 객체/JSONDeepCopy.js
@@ -1,3 +1,4 @@
+// 객체를 JSON 문법으로 표현된 문자열로 전환했다가 다시 JSON 객체로 바꿈
 var copyObjectViaJSON = function (target) {
     return JSON.parse(JSON.stringify(target));
 };
@@ -14,9 +15,9 @@ var obj = {
 
 var obj2 = copyObjectViaJSON(obj);
 
-obj.a = 3;
+obj2.a = 3;
 obj2.b.c = 4;
 obj.b.d[1] = 3;
 
-console.log(obj);
-console.log(obj2);
+console.log(obj); // a: 1, b: { c: null, d: [1,3], func1: f() }, func2: f() }
+console.log(obj2); // a: 3, b: { c:4, d: [1, 2] } }
